49914cab1fa7eda48242c964f81389ca32048fe3
linhuaqing <linhq2@wangsu.com>
2018-01-19 10:49:55 +0800

    添加导出excel的功能

--- src/js/detail.js
+++ src/js/detail.js
@@ -2,6 +2,8 @@ require('../lib/liMarquee.css')
 require('../css/detail.css')
 const $ = require('jquery')
 require('../../src/lib/jquery.liMarquee')
+require('../../src/lib/tableExport')
+require('../../src/lib/jquery.base64')
 module.exports = function () {
   $(function () {
     $('.dowebok').liMarquee({
@@ -11,5 +13,13 @@ module.exports = function () {
   $('#app').on('click', function () {
     // _toast.show('11111')
     console.log(1)
+  })
+  $('#table-export').on('click', function () {
+    // _toast.show('11111')
+    console.log(11111111)
+    $('#table-id').tableExport({
+      type: 'excel',
+      escape: 'false'
+    })
   })
 }
