ea009fe9a847a0220648c55939739ef6d47d9e8a
linhuaqing <linhq2@wangsu.com>
2018-01-23 04:48:04 +0800

    完成detail页面需要输出的页面的数据重组

--- routes/detail.js
+++ routes/detail.js
@@ -1,10 +1,26 @@
+/*
+ * 拼贴前端需要的数据
+ */
 'use strict'
-// const list = {
-//   name: 'hhhhh'
-// }
-// const data = require('../datas/list.json')
-const data = [[12345, "11111", "9999999", 2]]
+
+const config = require('../build/config')
+const sourcePath = config.listDir
+const { getList, hasTypeList } = require('./tools')
+
 module.exports = (req, res) => {
-  console.log(data)
-  res.render('detail', {data: data})
+  getList(sourcePath).then(result => {
+    const datalist = []
+    for (let i = 0; i < 7; i++) {
+      const item = {}
+      if (i === 0) {
+        item.name = '特等奖'
+      } else {
+        item.name = i + '等奖'
+      }
+      item.data = hasTypeList(result, i)
+      datalist.push(item) 
+    }
+    res.render('detail', {data: datalist})
+  })
+  
 }
